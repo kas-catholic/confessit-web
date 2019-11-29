e48a7784d845b84463a97f3bc48831a5f5784da9
Mike Kasberg <kas.catholic@gmail.com>
2019-11-29 13:43:17 -0700

    Bootstrap cols are working

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import ExamineList from './ExamineList';
 import SinsList from './SinsList';
 import Walkthrough from './Walkthrough';
 import sinsdb from './data/sinsdb_en';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
 
   return (
     <div className="App">
-      <ExamineList sinsdb={sinsdb} />
-      <SinsList />
-      <Walkthrough />
+      <Container>
+        <Row>
+          <Col lg="4">
+            <ExamineList sinsdb={sinsdb} />
+          </Col>
+          <Col lg="4">
+            <SinsList />
+          </Col>
+          <Col lg="4">
+            <Walkthrough />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
