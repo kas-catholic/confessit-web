69037731ee42b66aacf59b61e36bc975fa75e15c
Mike Kasberg <kas.catholic@gmail.com>
2019-12-14 15:46:21 -0700

    Add sin items

---
+++ src/CommandmentGroup.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Collapse from 'react-bootstrap/Collapse'
+
+class CommandmentGroup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: true};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => (
+      {open: !state.open}
+    ));
+  }
+
+  render() {
+    return <Card>
+      <Card.Header id="headingOne">
+        <Button onClick={this.handleClick} aria-controls="collapse-text">
+          <h2>{this.props.title}</h2>
+        </Button>
+          <p>{this.props.text}</p>
+      </Card.Header>
+  
+      <Collapse in={this.state.open} id="collapse-text">
+        {this.props.children}
+      </Collapse>
+    </Card>
+  }
+}
+
+export default CommandmentGroup;
--- src/ExamineList.js
+++ src/ExamineList.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import ExpandableListRow from './ExpandableListRow';
+import CommandmentGroup from './CommandmentGroup';
+import SinItem from './SinItem';
 
 class ExamineList extends React.Component {
-    render() {
-        let sinsdb = this.props.sinsdb;
+  render() {
+    let sinsdb = this.props.sinsdb;
 
-        let elements = sinsdb.commandments.map( c =>
-            <ExpandableListRow title={c.title} text={c.text} key={c.title} />
-        );
+    let sins = sinsdb.sins.reduce((obj, sin) => {
+      if (!obj.hasOwnProperty(sin.commandment_id)) {
+        obj[sin.commandment_id] = [];
+      }
 
-        return <div>
-            <h2>Examine List</h2>
-            {elements}
-        </div>;
-    }
+      obj[sin.commandment_id].push(<SinItem text={sin.text} key={sin.sin_id} />);
+
+      return obj;
+    }, {});
+      
+    let elements = sinsdb.commandments.map( c =>
+      <CommandmentGroup title={c.title} text={c.text} key={c.commandment_id}>
+        <div>
+          {sins[c.commandment_id]}
+        </div>
+      </CommandmentGroup>
+    );
+
+    return <div>
+      <h2>Examine List</h2>
+      {elements}
+    </div>;
+  }
 }
 
 export default ExamineList;
