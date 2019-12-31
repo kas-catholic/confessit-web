fd33dff3015dc1c4c9205e31c260bd4e25707521
Mike Kasberg <kas.catholic@gmail.com>
2019-12-31 10:22:34 -0700

    Kill useless constructor warning

--- src/SinsList.js
+++ src/SinsList.js
@@ -2,10 +2,6 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 
 class SinsList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  
   render() {
     let sinCards = this.props.sinsList.map((text, index) =>
       <Card key={index}>
