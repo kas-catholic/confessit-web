3a4696b63c13fcfcf1c1bd17b061bdeb6b8e807d
Mike Kasberg <kas.catholic@gmail.com>
2020-01-13 19:07:20 -0700

    Working React Router
    It is able to load a basic route correctly running on localhost.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Col from 'react-bootstrap/Col';
 import Swiper from 'react-id-swiper';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 class App extends React.Component {
   swiperParams = {
@@ -61,40 +62,49 @@ class App extends React.Component {
     let sinsList = this.buildSinsList();
 
     return (
-      <div className="App">
-        <Navbar sticky="top" bg="primary" expand="lg">
-          <Navbar.Brand className="text-white" href="#home"><h1>ConfessIt</h1></Navbar.Brand>
-          {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-          </Navbar.Collapse> */}
-        </Navbar>
-        <Container fluid={true}>
-          <Row className="h-100">
-            <Col xs="12" className="h-100">
-              <Swiper {...this.swiperParams}>
-                <div className="col-scroll">
-                  <ExamineList
-                    sinsdb={sinsdb}
-                    selectedSinIds={this.state.selectedSinIds}
-                    onAddSinId={this.addSinId}
-                    onRemoveSinId={this.removeSinId}
-                  />
-                </div>
-                <div className="col-scroll">
-                  <SinsList sinsList={sinsList} />
-                </div>
-                <div className="col-scroll">
-                  <Walkthrough sinsList={sinsList} />
-                </div>
-              </Swiper>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar sticky="top" bg="primary" expand="lg">
+            <Navbar.Brand className="text-white" href="#home"><h1>ConfessIt</h1></Navbar.Brand>
+            {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+              </Nav>
+            </Navbar.Collapse> */}
+          </Navbar>
+          <Switch>
+            <Route exact path="/">
+              <Container fluid={true}>
+                <Row className="h-100">
+                  <Col xs="12" className="h-100">
+                    <Swiper {...this.swiperParams}>
+                      <div className="col-scroll">
+                        <ExamineList
+                          sinsdb={sinsdb}
+                          selectedSinIds={this.state.selectedSinIds}
+                          onAddSinId={this.addSinId}
+                          onRemoveSinId={this.removeSinId}
+                        />
+                      </div>
+                      <div className="col-scroll">
+                        <SinsList sinsList={sinsList} />
+                      </div>
+                      <div className="col-scroll">
+                        <Walkthrough sinsList={sinsList} />
+                      </div>
+                    </Swiper>
+                  </Col>
+                </Row>
+              </Container>
+            </Route>
+            <Route path="/about">
+              <p>About</p>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
