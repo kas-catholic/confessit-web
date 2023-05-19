93ffc274b73e637d9d6fce4b41083cc2baea7a46
John R. D'Orazio <priest@johnromanodorazio.com>
2023-05-19 18:46:46 +0200

    move {t} to correct position

--- src/PriestBubble.js
+++ src/PriestBubble.js
@@ -3,8 +3,8 @@ import Card from "react-bootstrap/Card";
 import { withTranslation } from "react-i18next";
 
 class PriestBubbleComponent extends React.Component {
-  const { t } = this.props;
   render() {
+    const { t } = this.props;
     return <Card className="priest-bubble">{t("priestbubble.priest")}: {this.props.text}</Card>;
   }
 }
