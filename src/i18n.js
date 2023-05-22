7b2f9f165e797666c29bb6cba5fab98dbf984f9b
John R. D'Orazio <priest@johnromanodorazio.com>
2023-05-22 14:03:50 +0200

    attempt keep html nodes i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -22,6 +22,24 @@ i18n
     // allow an empty value to count as invalid
     // https://www.i18next.com/principles/fallback#missing-values-for-existing-keys
     returnEmptyString: false,
+    react: {
+      transSupportBasicHtmlNodes: true,
+      transKeepBasicHtmlNodesFor: [
+        "br",
+        "strong",
+        "i",
+        "p",
+        "vatican",
+        "github",
+        "mass",
+        "osc",
+        "website",
+        "app",
+        "a",
+        "kbd",
+        "code",
+      ],
+    },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
