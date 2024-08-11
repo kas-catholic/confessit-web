725cd5d3889e0b2beb015a1f67245bcc7e7efb10
Mike Kasberg <kasberg.mike@gmail.com>
2024-08-10 19:42:53 -0600

    Add Vite PWA
    https://github.com/vite-pwa/astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import AstroPWA from '@vite-pwa/astro'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), AstroPWA()]
 });
