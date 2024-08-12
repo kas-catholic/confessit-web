7492167681177342c38a47f35828844322b6a1f7
Mike Kasberg <kasberg.mike@gmail.com>
2024-08-11 19:45:15 -0600

    Got Existing Translations Working!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-
-import AstroPWA from '@vite-pwa/astro'
+import AstroPWA from '@vite-pwa/astro';
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), AstroPWA()],
+  integrations: [react(), tailwind(), AstroPWA(), astroI18next()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"]
