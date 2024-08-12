dbc35ba5b175e3251d4b9efd235d75b8959be1ee
Mike Kasberg <kasberg.mike@gmail.com>
2024-08-11 20:04:11 -0600

    Get Tailwind Typography Working

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui"
+import typography from "@tailwindcss/typography"
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [daisyui],
+	plugins: [typography, daisyui],
 	daisyui: {
 		themes: [
 			{
