760b3783e70dea77e144b6c5b86bfbdb969bef1c
Mike Kasberg <kasberg.mike@gmail.com>
2024-11-03 14:51:59 -0700

    Examine List Rendering Correctly (not styled)
    Implemented the Examine List, things seem to be working correctly with
    translations! Ripped out the old Bootstrap styles from the non-Astro
    version, we'll re-implement styles with daisyUI and tailwind after I get
    the basic functionality in place.

---
+++ src/components/ExamineItem.jsx
@@ -0,0 +1,30 @@
+import { t } from "i18next";
+
+const ExamineItem = ({ text, sinId, selected, onAddSinId, onRemoveSinItem }) => {
+  const handleClick = (e) => {
+    if (e.target.checked) {
+      onAddSinId(sinId);
+    } else {
+      onRemoveSinItem({ id: sinId });
+    }
+  };
+
+  return (
+    <div className="bg-green-200">
+      {text}
+      <div className="md-checkbox">
+        <input
+          id={`checkitem:${sinId}`}
+          type="checkbox"
+          checked={selected}
+          onChange={handleClick}
+        />
+        <label htmlFor={`checkitem:${sinId}`}>
+          {t("examineitem.yes")}
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default ExamineItem;
