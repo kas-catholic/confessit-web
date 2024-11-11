e5cf63238d8f1b17c74001181d9677d143b56328
Mike Kasberg <kasberg.mike@gmail.com>
2024-11-10 19:19:34 -0700

    SinList is working!
    Next up will be making the walkthrough functional (unstyled).

--- src/components/SinsList.jsx
+++ src/components/SinsList.jsx
@@ -1,7 +1,21 @@
-function SinsList() {
+import { t } from "i18next";
+
+import SinListItem from "@components/SinListItem";
+
+const SinsList = ({ sinsList, onRemoveSinItem }) => {
+  const sinCards = sinsList.map((sinItem, index) => (
+    <SinListItem
+      sinItem={sinItem}
+      onRemoveSinItem={onRemoveSinItem}
+    />
+  ));
+
   return (
-    <h2>Sins List</h2>
+    <div>
+      <h2 className="mt-5">{t("sins_list.review")}</h2>
+      {sinCards}
+    </div>
   );
-}
+};
 
 export default SinsList;
