631564cb7c2922d8a0e573253a6cc44cf9e2b991
Mike Kasberg <kas.catholic@gmail.com>
2024-11-25 21:21:45 -0700

    Review Styles First Pass
    * Replace astro-heroicons with heroicons/react. The react one is
      official, and the react one works with astro, but not vice-versa on
      client-side react.
    * Use flex box to style the review items. Might play around some more
      with colors and borders but this is good for now...

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,8 +1,12 @@
 const Column = ({ title, children }) => {
   return (
     <div className="h-full overflow-y-auto">
-      <h2 className="text-4xl font-bold text-center my-12 mx-auto">{title}</h2>
-      {children}
+      <div className="mx-4">
+        <h2 className="text-4xl font-bold text-center my-12 mx-auto">
+          {title}
+        </h2>
+        {children}
+      </div>
     </div>
   );
 };
