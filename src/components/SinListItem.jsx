697d24f3a6d8ff96b284960274cea3dabb62a801
Mike Kasberg <kas.catholic@gmail.com>
2024-11-25 21:21:45 -0700

    Review Styles First Pass
    * Replace astro-heroicons with heroicons/react. The react one is
      official, and the react one works with astro, but not vice-versa on
      client-side react.
    * Use flex box to style the review items. Might play around some more
      with colors and borders but this is good for now...

--- src/components/SinListItem.jsx
+++ src/components/SinListItem.jsx
@@ -1,13 +1,17 @@
+import { TrashIcon } from "@heroicons/react/16/solid";
+
 const SinListItem = ({ sinItem, onRemoveSinItem }) => {
   const handleDelete = () => {
     onRemoveSinItem(sinItem);
   };
 
   return (
-    <div className="bg-yellow-300">
-      {sinItem.text}
-      <a className="mt-2 btn btn-error" size="sm" onClick={handleDelete}>
-        <i className="fa fa-trash"></i>
+    <div className="mb-4 bg-base-200 rounded-md flex flex-row">
+      <div className="flex-1 p-2 flex flex-col justify-center">
+        {sinItem.text}
+      </div>
+      <a className="btn btn-error h-full w-12" onClick={handleDelete}>
+        <TrashIcon className="h-5" />
       </a>
     </div>
   );
