d936217ba1d35c94caf878a9cc215831079ef27a
Mike Kasberg <kas.catholic@gmail.com>
2024-11-27 21:38:46 -0700

    Examine List Collapse

--- src/components/CommandmentGroup.jsx
+++ src/components/CommandmentGroup.jsx
@@ -4,13 +4,14 @@ const CommandmentGroup = ({ title, text, cmdId, children }) => {
   // TODO: Use a daisyUI Collapse component to make this group collapsible.
   // Store any necessary collapse state in this component.
   return (
-    <div className="bg-red-600">
-      <div className="bg-blue-400">
-        <h3>{title}</h3>
+    <div className="collapse collapse-arrow mb-8 bg-base-200">
+      <input type="checkbox" defaultChecked />
+      <div className="collapse-title">
+        <h3 className="text-2xl">{title}</h3>
         <p>{text}</p>
       </div>
 
-      <div>{children}</div>
+      <div className="collapse-content">{children}</div>
     </div>
   );
 };
