981d507be894f68fa57a2ffc5f0e92bcbd60a765
Mike Kasberg <kas.catholic@gmail.com>
2024-11-30 14:09:20 -0700

    Implement Add sin modal

---
+++ src/components/AddSinModal.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { t } from "i18next";
+
+const AddSinModal = ({ addCustomSin }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleCancel = () => {
+    setInputValue("");
+  };
+
+  const handleSubmit = () => {
+    addCustomSin(inputValue);
+    setInputValue("");
+  };
+
+  return (
+    <dialog id="AddSinModal" className="modal modal-bottom lg:modal-middle">
+      <div className="modal-box">
+        <h3 className="text-lg font-bold">{t("addbutton.add-custom-sin")}</h3>
+        <textarea
+          className="my-4 textarea textarea-bordered w-full"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder={t("addbutton.i-sinned-by")}
+        ></textarea>
+        <div className="modal-action">
+          <form method="dialog">
+            <button className="btn" onClick={handleCancel}>
+              {t("addbutton.cancel")}
+            </button>
+          </form>
+          <form method="dialog">
+            <button className="btn btn-primary" onClick={handleSubmit}>
+              {t("addbutton.add")}
+            </button>
+          </form>
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default AddSinModal;
