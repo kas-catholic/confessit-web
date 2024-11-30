5f70495aa13c0f287bf5d21a1b9dcaef22b7748c
Mike Kasberg <kas.catholic@gmail.com>
2024-11-29 21:40:16 -0700

    Color chat bubbles differently

--- src/components/SpeechBubble.jsx
+++ src/components/SpeechBubble.jsx
@@ -2,13 +2,14 @@ import { t } from "i18next";
 
 const SpeechBubble = ({ isPriest, children }) => {
   const clazz = isPriest ? "chat-start" : "chat-end";
+  const bubbleClass = isPriest ? "chat-bubble-secondary" : "chat-bubble-accent";
 
   return (
     <div className={`chat mb-4 ${clazz}`}>
       {isPriest && (
         <div className="chat-header">{t("priestbubble.priest")}</div>
       )}
-      <div className="chat-bubble chat-bubble-secondary">{children}</div>
+      <div className={`chat-bubble ${bubbleClass}`}>{children}</div>
     </div>
   );
 };
