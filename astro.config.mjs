0522e806bf33aa3d0fa6810db29dc7b20c5e7421
Mike Kasberg <kas.catholic@gmail.com>
2025-03-02 07:12:18 -0700

    Update DaisyUI to 5.0
    Following https://daisyui.com/docs/upgrade/
    
    Still need to put themes and typography back in the new config format.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import AstroPWA from "@vite-pwa/astro";
 import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [
-    react(),
-    tailwind({ applyBaseStyles: false }),
-    AstroPWA(),
-    astroI18next(),
-  ],
+  integrations: [react(), AstroPWA(), astroI18next()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
