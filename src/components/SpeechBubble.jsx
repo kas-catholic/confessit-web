78a177057bc3044a9bbb7823a9fd77de3032690f
Mike Kasberg <kas.catholic@gmail.com>
2025-03-02 12:05:35 -0700

    Improve Chat Bubble Colors
    DaisyUI 5 supports using base color chat bubbles, so we don't need to
    use primary to distinguish the user bubble and can instead use something
    a bit more subtle.

--- src/components/SpeechBubble.jsx
+++ src/components/SpeechBubble.jsx
@@ -2,7 +2,7 @@ import { t } from "i18next";
 
 const SpeechBubble = ({ isPriest, children }) => {
   const clazz = isPriest ? "chat-start" : "chat-end font-bold";
-  const bubbleClass = isPriest ? "" : " chat-bubble-primary";
+  const bubbleClass = isPriest ? " chat-bubble-neutral" : "";
 
   return (
     <div className={`chat mb-4 ${clazz}`}>
