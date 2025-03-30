f8dcf63d60681ba12820eff3b5362b450672f326
Mike Kasberg <kas.catholic@gmail.com>
2025-03-30 10:58:27 -0600

    Set up PWA Correctly
    Configure the PWA correctly using VitePWA/AstroPWA.
    
    Including fixing the web manifest and favicons!

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,14 @@ import astroI18next from "astro-i18next";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [react(), AstroPWA(), astroI18next()],
+  integrations: [
+    react(),
+    AstroPWA({
+      registerType: "autoUpdate",
+      manifest: false,
+    }),
+    astroI18next(),
+  ],
   i18n: {
     defaultLocale: "en",
     locales: ["de", "en", "es", "it", "pt-BR"],
