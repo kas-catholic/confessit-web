45ae8514b08e736f8446fa602f6606a0ee2f73f9
Mike Kasberg <kas.catholic@gmail.com>
2025-03-30 11:19:55 -0600

    Add a sitemap
    Using the astro sitemap plugin
    
    https://docs.astro.build/en/guides/integrations-guide/sitemap/

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import AstroPWA from "@vite-pwa/astro";
 import astroI18next from "astro-i18next";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://confessit.app",
   output: "static",
   integrations: [
     react(),
@@ -14,6 +16,18 @@ export default defineConfig({
       manifest: false,
     }),
     astroI18next(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          es: "es-US",
+          de: "de-DE",
+          it: "it-IT",
+          "pt-BR": "pt-BR",
+        },
+      },
+    }),
   ],
   i18n: {
     defaultLocale: "en",
